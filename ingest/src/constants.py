0ae53f08dfdf8cc31af9bac214a7b121f6bb6938
rohanakut <rohan.akut97@gmail.com>
2025-05-30 08:51:22 -0400

    UPdated to final folder structure

--- ingest/src/constants.py
+++
@@ -1,3 +0,0 @@
-repo_path = "/Users/rohanakut/Desktop/onboardAI/django"
-CHECKPOINT_FILE = "../../../ingest_records.jsonl"
-PROGRESS_FILE   = "../../../ingest_progress.json"
---
+++ training/ingest/src/constants.py
@@ -0,0 +1,3 @@
+repo_path = "/Users/rohanakut/Desktop/onboardAI/django"
+CHECKPOINT_FILE = "/Users/rohanakut/Desktop/onboardAI/training/ingest/checkpoints/ingest_records.jsonl"
+PROGRESS_FILE   = "/Users/rohanakut/Desktop/onboardAI/training/ingest/checkpoints/ingest_progress.json"
